0cac13e1ea78e62980612922d08f94fa93e2e1e3
Xavier Paredes <jxavierpp1@gmail.com>
2020-05-30 14:20:51 -0700

    subjectCard component created

--- src/App.js
+++ src/App.js
@@ -3,15 +3,7 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
-// import AsignatureCard from './components/AsignatureCard'
-
-const Ejes = {
-  BASICO: 0,
-  COMUN: 1,
-  PROFESIONAL: 2,
-  ESPECIALIZANTE: 3,
-  INTEGRADOR: 4
-}
+import AsignatureCard from './components/AsignatureCard'
 
 export default class App extends React.Component {
   constructor(props) {
@@ -21,14 +13,14 @@ export default class App extends React.Component {
   }
 
   loadData() {
-    fetch('http://www.mocky.io/v2/5ed0d4533500004a00ff9f9e')
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        this.setState({ data: data })
-      })
-      .catch(err => console.error(this.props.url, err.toString()))
-  }
+      fetch('http://www.mocky.io/v2/5ed0d4533500004a00ff9f9e')
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          this.setState({ data: data })
+        })
+        .catch(err => console.error(this.props.url, err.toString()))
+    }
 
   componentDidMount() {
     this.loadData()
@@ -37,30 +29,14 @@ export default class App extends React.Component {
   renderAsignaturas(materias) {
     const items = [];
     for (const [index, materia] of materias.entries()) {
-      let color;
-      if(materia.eje === Ejes.BASICO) {
-        color = "#F8F8F9";
-      }
-      if(materia.eje === Ejes.COMUN) {
-        color = "#F0F061";
-      }
-      if(materia.eje === Ejes.PROFESIONAL) {
-        color = "#FB9B63";
-      }
-      if(materia.eje === Ejes.ESPECIALIZANTE) {
-        color = "#98F662";
-      }
-      if(materia.eje === Ejes.INTEGRADOR) {
-        color = "#9962F8";
-      }
-      items.push(<div className="col" key={index} style={{backgroundColor: color}}><p>{materia.asignatura}</p></div>)
+      items.push(<AsignatureCard className="col" key={index} name={materia.asignatura} eje={materia.eje} isApproved={this.state.isShow} />)
     }
     return items;
   }
 
   render() {
     return (
-      <Container>
+      <Container fluid="xl">
         <div className="container1">
           { 
             this.state.data.map((materias, i) => {
@@ -73,31 +49,18 @@ export default class App extends React.Component {
               )
             })
           }
-        </div>
-        {/* <div className="menu">
-          as
-        </div> */}
+        </div >
+        <div className="container2">
+            <div className="row1">
+              <div>MATERIAS EJE ESPECIALIZANTE</div>
+              <Col>2 of 2</Col>
+            </div>
+            <div className="row1">
+              <div>MATERIAS EJE INTEGRADOR</div>
+              <Col>2 of 2</Col>
+            </div>
+          </div>
       </Container>
     )
   }
-
-  // render() {
-  //   return(
-  //     <div className={this.classes.root}>
-  //       <Grid container spacing={3}>
-  //         { 
-  //           this.state.data.map((materias, i) => {
-  //             return ( 
-  //               <Grid item xs={1} key={i} >
-  //                 { 
-  //                   this.renderAsignaturas(materias)
-  //                 }
-  //               </Grid>
-  //             )
-  //           })
-  //         }
-  //       </Grid>
-  //     </div>
-  //   )
-  // }
 }
--- src/components/AsignatureCard.js
+++ src/components/AsignatureCard.js
@@ -1,8 +1,33 @@
 import React from 'react';
 
+const Ejes = {
+  BASICO: 0,
+  COMUN: 1,
+  PROFESIONAL: 2,
+  ESPECIALIZANTE: 3,
+  INTEGRADOR: 4
+}
+
 export default class AsignatureCard extends React.Component {
 
     render() {
+      let color;
+      if(materia.eje === Ejes.BASICO) {
+        color = "#F8F8F9";
+      }
+      if(materia.eje === Ejes.COMUN) {
+        color = "#F0F061";
+      }
+      if(materia.eje === Ejes.PROFESIONAL) {
+        color = "#FB9B63";
+      }
+      if(materia.eje === Ejes.ESPECIALIZANTE) {
+        color = "#98F662";
+      }
+      if(materia.eje === Ejes.INTEGRADOR) {
+        color = "#9962F8";
+      }
+
       return (
         <div>
             MATERIA
