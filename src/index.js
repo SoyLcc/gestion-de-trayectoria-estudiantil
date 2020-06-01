06a34cfd7ec14082740cab67eeea8ba0b747eb94
Xavier Paredes <jxavierpp1@gmail.com>
2020-06-01 03:27:36 -0700

    Header.js removed from index

--- src/index.js
+++ src/index.js
@@ -6,14 +6,9 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from './App';
-import Header from './components/Header';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Header
-      background="background-image: linear-gradient(116deg, #08AEEA 0%, #2AF598 100%)"
-      title="SoyLcc"
-    />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
