b37bd463a6ea4c9541e2e0540bb5599a1d2168b0
Xavier Paredes <jxavierpp1@gmail.com>
2020-06-01 03:26:10 -0700

    react routes added

--- src/App.js
+++ src/App.js
@@ -1,66 +1,21 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 
-import SubjectCard from './components/SubjectCard'
+import Navigation from './components/Navigation';
+import StudentPlan from './components/StudentPlan';
+import Poll from './components/Poll';
+import Profile from './components/Profile';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { data: [] }
-
-  }
-
-  loadData() {
-      fetch('http://www.mocky.io/v2/5ed4700a3300005f00f7a191')
-        .then(response => response.json())
-        .then(data => {
-          console.log(data);
-          this.setState({ data: data })
-        })
-        .catch(err => console.error(this.props.url, err.toString()))
-    }
-
-  componentDidMount() {
-    this.loadData()
-  }
-
-  renderSubjects(subjects) {
-    const items = [];
-    for (const [index, subject] of subjects.entries()) {
-      items.push(<SubjectCard className="col" key={index} subject={subject} />)
-    }
-    return items;
-  }
 
   render() {
     return (
-      <Container fluid="xl">
-        <div className="container1">
-          { 
-            this.state.data.map((subjects, i) => {
-              return ( 
-                <div className="row1" key={i} >
-                  { 
-                    this.renderSubjects(subjects)
-                  }
-                </div>
-              )
-            })
-          }
-        </div >
-        <div className="container2">
-            <div className="row1">
-              <div>MATERIAS EJE ESPECIALIZANTE</div>
-              <Col>2 of 2</Col>
-            </div>
-            <div className="row1">
-              <div>MATERIAS EJE INTEGRADOR</div>
-              <Col>2 of 2</Col>
-            </div>
-          </div>
-      </Container>
+      <Router>
+        <Navigation/>
+        <Route path="/" exact  component={ StudentPlan } />
+        <Route path="/poll" component={ Poll } />
+        <Route path="/Profile" component={ Profile } />
+      </Router>
     )
   }
 }
