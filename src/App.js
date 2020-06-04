0b9159fff2e7a4b8a8d4501d7c4b21450034c32f
Xavier Paredes <jxavierpp1@gmail.com>
2020-06-03 20:39:23 -0700

    renaming profile component to ProfileHome

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 
 import Navigation from './components/Navigation';
-import StudentHome from './components/student';
-import Profile from './components/Profile';
+import StudentHome from './components/student/StudentHome';
+import ProfileHome from './components/profile/ProfileHome';
 import PollsHome from './components/polls/PollsHome';
 
 export default class App extends React.Component {
@@ -14,7 +14,7 @@ export default class App extends React.Component {
         <Navigation/>
         <Route path="/" exact  component={ StudentHome } />
         <Route path="/poll" component={ PollsHome } />
-        <Route path="/Profile" component={ Profile } />
+        <Route path="/Profile" component={ ProfileHome } />
       </Router>
     )
   }
--- src/components/profile/ProfileHome.js
+++ src/components/profile/ProfileHome.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 
-export default class Profile extends Component {
+export default class ProfileHome extends Component {
     render() {
         return (
             <div>
-                Profile
+                Profile Home
             </div>
         )
     }
