0886cfe8c3473157e05f9cb36c432984c4d7ee98
Xavier Paredes <jxavierpp1@gmail.com>
2020-06-03 19:55:31 -0700

    general components intialized, content component

---
+++ src/components/utils.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+// Here will be the components that can be used in every page/component.
+
+const Content = styled.div`
+    width: 100%;
+    padding: 20px;  
+    background-color: #e4f6f3;
+`;
+
+export default Content;
