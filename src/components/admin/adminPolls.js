b63589eb9eef5fbaf2375522ba79efffdc58f487
Xavier Paredes <jxavierpp1@gmail.com>
2020-06-06 16:14:18 -0700

    read polls connected with api endpoint

--- src/components/admin/adminPolls.js
+++ src/components/admin/adminPolls.js
@@ -1,11 +1,53 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+
 import NavigationLeft from './NavigationLeft'
 import Content from '../utils'
 import Table from 'react-bootstrap/Table'
 import Button from 'react-bootstrap/Button'
 
+
 export default class adminPolls extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { 
+            polls: [], 
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    loadData = async () => {
+        const res = await axios.get('http://localhost:4000/api/polls/');
+        this.setState({polls:res.data});
+    }
+    updateData = async () => {
+        const res = await axios.update('http://localhost:4000/api/polls/', {});
+        this.loadData();
+    }
+    
+    componentDidMount() {
+        this.loadData();
+    }
+    
+    async handleClick(id) {
+        await axios.delete('http://localhost:4000/api/polls/'+id)
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        this.loadData();
+    }
     render() {
+        function OpenCloseButton(props) {
+            const isActive = props.isActive;
+            if (isActive) {
+              return <Button variant="primary" size="sm">Cerrar</Button>;
+            }
+            return <Button variant="primary" size="sm">Abrir</Button>;
+        }
         return (
             <div className="container-fluid">
                 <div className="row">
@@ -14,50 +56,41 @@ export default class adminPolls extends Component {
                         <div className="container">
                             <div className="row">
                             <div className="col-md-2"></div>
-                                <div className="col-md-8">
+                                <div className="col-md-9">
                                     <div class="card text-center" style={{border: "1px solid rgba(0,0,0,.160)"}}>
                                         <h5 class="card-header">CRUD Encuestas </h5>
                                         <div class="card-body">
-                                            <Button variant="success" size="sm" className="float-right mb-2">Crear Encuesta</Button>
+                                            <Link to="/admin/newpoll">
+                                                <Button variant="success" size="sm" className="float-right mb-2">Crear Encuesta</Button>
+                                            </Link>
                                             <Table striped bordered hover variant="dark" size="sm">
                                                 <thead>
                                                     <tr>
                                                         <th>#</th>
                                                         <th>Titulo</th>
+                                                        <th>Descripción</th>
                                                         <th>Acciones</th>
                                                     </tr>
                                                 </thead>
                                                 <tbody>
-                                                    <tr>
-                                                        <td>1</td>
-                                                        <td>2020-1</td>
-                                                        <td>
-                                                            <Button variant="primary" size="sm">Cerrar/Abrir</Button>{' '}
-                                                            <Button variant="primary" size="sm">Resultados</Button>{' '}
-                                                            <Button variant="warning" size="sm">Editar</Button>{' '}
-                                                            <Button variant="danger" size="sm">Eliminar</Button>
-                                                        </td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>2</td>
-                                                        <td>2019-2</td>
-                                                        <td>
-                                                            <Button variant="primary" size="sm">Cerrar/Abrir</Button>{' '}
-                                                            <Button variant="primary" size="sm">Resultados</Button>{' '}
-                                                            <Button variant="warning" size="sm">Editar</Button>{' '}
-                                                            <Button variant="danger" size="sm">Eliminar</Button>
-                                                        </td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>3</td>
-                                                        <td>2019-1</td>
-                                                        <td>
-                                                            <Button variant="primary" size="sm">Cerrar/Abrir</Button>{' '}
-                                                            <Button variant="primary" size="sm">Resultados</Button>{' '}
-                                                            <Button variant="warning" size="sm">Editar</Button>{' '}
-                                                            <Button variant="danger" size="sm">Eliminar</Button>
-                                                        </td>   
-                                                    </tr>
+                                                    {
+                                                        this.state.polls.map((poll, i) => {
+                                                            return (
+                                                                <tr key={i}>
+                                                                    <td>{i}</td>
+                                                                    <td>{poll.title}</td>
+                                                                    <td>{poll.description}</td>
+                                                                    <td>
+                                                                        <OpenCloseButton isActive={poll.isActive}/>
+                                                                        {' '}
+                                                                        <Button variant="primary" size="sm">Resultados</Button>{' '}
+                                                                        <Button variant="warning" size="sm">Editar</Button>{' '}
+                                                                        <Button variant="danger" size="sm" onClick={() => { if (window.confirm('Usted confirma esta acción?')) this.handleClick(poll._id) } }>Eliminar</Button>
+                                                                    </td>
+                                                                </tr>
+                                                            )
+                                                        })
+                                                    }
                                                 </tbody>
                                             </Table>
                                         </div>
