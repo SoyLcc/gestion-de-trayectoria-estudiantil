8006542ac6aa031db53257f8f6a44f0f19957271
Xavier Paredes <jxavierpp1@gmail.com>
2020-06-05 20:34:59 -0700

    new poll formulary layout improved

--- src/components/admin/polls/newpoll.js
+++ src/components/admin/polls/newpoll.js
@@ -22,8 +22,18 @@ export default class newPoll extends Component {
                             <div className="col">
                                 <div className="card text-center" style={{border: "1px solid rgba(0,0,0,.160)"}}>
                                     <h5 className="card-header">Nueva Encuesta </h5>
+                                    <div className="card-body pb-0">
+                                        <form>
+                                            <div className="form-group row">
+                                                <label for="staticEmail" className="col-sm-2 col-form-label">Titulo de la Encuesta:</label>
+                                                <div className="col-sm-10">
+                                                    <input type="text" className="form-control"/>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
                                     <div className="card-body" style={{display:"flex"}}>
-                                        <NewPollBoard/>
+                                            <NewPollBoard/>
                                     </div>
                                     <div className="card-footer">
                                         <Button variant="success" size="sm" className="float-right mb-2">Crear Encuesta</Button>
