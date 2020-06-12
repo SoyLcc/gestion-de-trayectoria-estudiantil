26bdd0826afc924e2737c0c765a554cc6f2df9a3
Xavier Paredes <jxavierpp1@gmail.com>
2020-06-11 18:13:29 -0700

    PollsHome changed

--- src/components/polls/PollsHome.js
+++ src/components/polls/PollsHome.js
@@ -1,67 +1,64 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 // import styled from 'styled-components';
 import Content from '../utils';
 
-function PollsClosed() {
-    return (
-        <div class="card text-center" style={{border: "1px solid rgba(0,0,0,.160)"}}>
-            <h5 class="card-header">Atencion Estudiantes!</h5>
-            <div class="card-body">
-                <h5 class="card-title">Próxima Encuesta Semestral</h5>
-                <p class="card-text">Comunidad de Lcc la proxima encuesta semestral para concer las necesidades de los estudiantes se llevara acabo en el siguiente periodo:</p>
-                <p class="card-text font-weight-bold">25 de agosto de 2020 - 7 de septiembre de 2020</p>
-            </div>
-        </div>
-    );
-}
-
-function PollsOpened() {
-    return (
-        <div class="card text-center" style={{border: "1px solid rgba(0,0,0,.160)"}}>
-            <h5 class="card-header">Atencion Estudiantes!</h5>
-            <div class="card-body">
-                <h5 class="card-title">Encuesta Semestral Abierta</h5>
-                <p class="card-text">Comunidad de Lcc la proxima encuesta semestral para concer las necesidades de los estudiantes esta abierta</p>
-                <a href="#" class="btn btn-primary">ir a la Encuesta</a>
-            </div>
-        </div>
-    );
-}
-function LastPollReuslts() {
-    return (
-        <div class="card text-center" style={{border: "1px solid rgba(0,0,0,.160)"}}>
-            <h5 class="card-header">Resultados Encuesta 2020-1</h5>
-            <div class="card-body">
-                <h5 class="card-title">Resultados</h5>
-                <p class="card-text">Resultados...</p>
-            </div>
-        </div>
-    );
-}
-
 export default class PollsHome extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { polls: [] }
+    }
+    
+    async loadData() {
+        try {
+            const response = await axios({
+                method: 'GET',
+                url: 'http://localhost:4000/api/polls',
+            });
+
+            if (response.status === 200) {
+                console.log(response.data);
+                this.setState({
+                    polls: response.data
+                })
+            }
+        } catch (error) {
+            
+        }
+    }
+    
+    componentDidMount() {
+        this.loadData()
+    }
+
+    PollCard(poll) {
+        return (
+            <div className="card text-center" style={{border: "1px solid rgba(0,0,0,.160)"}}>
+                <h5 className="card-header">{poll.title}</h5>
+                <div className="card-body">
+                    <p className="card-text">{poll.description}</p>
+                    <a href="#" className="btn btn-primary">Abrir Encuesta</a>
+                </div>
+            </div>
+        );
+    }
+
     render() {
         return (
             <Content>
                 <div className="container">
-                    <div className="row">
-                        <div className="col-md-2"></div>
-                        <div className="col-md-8">
-                            { PollsClosed() }
-                        </div>
-                    </div>
-                    <div className="row pt-5">
-                        <div className="col-md-2"></div>
-                        <div className="col-md-8">
-                            { PollsOpened() }
-                        </div>
-                    </div>
-                    <div className="row pt-5">
-                        <div className="col-md-2"></div>
-                        <div className="col-md-8">
-                            { LastPollReuslts() }
-                        </div>
-                    </div>
+                    {
+                        this.state.polls.map((poll, i) => {
+                            return (
+                                <div className="row pt-5"  key={i}>
+                                    <div className="col-md-2"></div>
+                                    <div className="col-md-8">
+                                        { this.PollCard(poll) }
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
             </Content>
         )
