5c1d3f0817a1bb7f1ef37f39751ee6b9ab058cb6
Xavier Paredes <jxavierpp1@gmail.com>
2020-06-20 19:46:13 -0700

    fixing margin

--- src/components/student/StudentHome.js
+++ src/components/student/StudentHome.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react'
 // import styled from 'styled-components';
 import StudentPlan from './StudentPlan'
+import Content from '../utils';
 
 export default class StudentHome extends Component {
     render() {
         return (
-            <div className="row">
-                <div className="col-md-2"></div>
-                <div className="col-md-8">
+            <Content>
+                <div className="container">
                     <StudentPlan/>
                 </div>
-            </div>
+            </Content>
         )
     }
 }
