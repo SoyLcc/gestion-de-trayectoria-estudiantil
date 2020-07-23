9a2c6174c8d6c47f91c129704b142014ec4af566
Xavier Paredes <jxavierpp1@gmail.com>
2020-07-23 16:56:54 -0700

    admin panel link added for admins

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,7 +6,19 @@ import Nav from 'react-bootstrap/Nav'
 import './navigationStyles.css'
 
 export default class Navigation extends Component {
+
+    componentDidMount() {
+        this.checkUserprivileges()
+    }
+
+    checkUserprivileges(){
+        
+
+    }
+
     render() {
+        let isAdmin = localStorage.getItem("isAdmin")
+
         return (
             <Navbar collapseOnSelect expand="md" bg="dark" variant="dark" fixed="top">
                 <Link to="/">
@@ -17,7 +29,10 @@ export default class Navigation extends Component {
                     <Nav className="ml-auto">
                         <Link className="nav-link" to="/">Inicio</Link>
                         <Link className="nav-link" to="/poll">Encuestas</Link>
-                        <Link className="nav-link" to="/profile">Mi Perfil</Link>
+                        {/* <Link className="nav-link" to="/profile">Mi Perfil</Link> */}
+                        {isAdmin ?
+                            <Link className="nav-link btn btn-success ml-2" to="/admin/polls">Admin Panel</Link>: ""
+                        }
                         <Link className="nav-link btn btn-secondary ml-2" to="/logout">Cerrar sesión</Link>
                     </Nav>
                 </Navbar.Collapse>
